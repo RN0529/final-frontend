59568f454f9b7d25c028922b243de0d6854534da
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-05-23 04:27:58 -0400

    added informative message to show no courses belong to instructor

--- src/components/views/InstructorView.js
+++ src/components/views/InstructorView.js
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
+import allInstructors from "../../store/reducers/instructors";
 
 
 const InstructorView = (props) => {
   const {instructor, editCourse, allCourses} = props;
   let assignedCourses = allCourses.filter(course => course.instructorId===instructor.id);
   let availableCourses = allCourses.filter(course => course.instructorId!==instructor.id);
-  
   return (
     <div>      
       <h1>{instructor.firstname} {instructor.lastname}</h1>
       <h3>{instructor.department}</h3>
       <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
         <div>Assigned courses:
-        {assignedCourses.map( course => {
+        {assignedCourses.length > 0 ? assignedCourses.map( course => {
           return (
             <div key={course.id}>
             <Link to={`/courses/${course.id}`}>
@@ -21,7 +21,7 @@ const InstructorView = (props) => {
             <button onClick={() => editCourse({id:course.id, instructorId: null})}>x</button>
             </div>
           );
-        })}</div>
+        }) : <p>no courses belong to this instructor</p>}</div>
         <div>Available courses:
         {availableCourses.map( course => {
           return (
